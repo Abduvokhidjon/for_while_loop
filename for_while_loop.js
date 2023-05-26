1e48b1c00e0ec504e969ece0e7d053201aef342a
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-26 16:36:59 -0500

    Final version

--- for_while_loop.js
+++ for_while_loop.js
@@ -32,29 +32,39 @@ Write a "for" loop that will count and print 1 to 10, once finished counting, pr
 for (var i = 1; i < 11; i ++) {
     console.log(i)
 }
-console.log("Finished counting to 10")
+console.log("Finished counting to 10");
 
 // Task 2
 // Write a "for" loop that will count and print 10 to 1, once finished counting, print "Finished counting to 1"
 for (i = 10; i > 0; --i) {
     console.log(i)
 }
-console.log("Finished counting to 1")
-
-
-
+console.log("Finished counting to 1");
 
 // Task 3
 // Re do the Task 1 with while loop, use different methods of incrementing/decrementing. comment out other methods of incrementing/decrementing.
 
+var i = 1
+while (i <= 10) {
+    console.log(i)
+    i = i + 1
+}
+console.log("Finished counting to 10");
+
 // Task 4
 // Re do the Task 2 with while loop, use different methods of incrementing/decrementing. comment out other methods of incrementing/decrementing.
 
+var i = 10
 
+while (i >= 1) {
+    console.log(i)
+    i = i - 1
+}
+console.log("Finished counting to 1");
 
-
-
-/* With the loop of your choice, from numbers 1 to 100, print out ONLY numbers that are divisible by BOTH 3 AND 5, not OR. 
+/* 
+Task 5
+With the loop of your choice, from numbers 1 to 100, print out ONLY numbers that are divisible by BOTH 3 AND 5, not OR. 
 Once finished, print out "DONE!"
 NOTE. You can increment the count only by one
 */
@@ -63,7 +73,8 @@ for (var i = 1; i < 101; i++)
 
 if (i % 3 == 0 && i % 5 ==0 ) {
     console.log(i)
-} else {}
+} else {};
+console.log("DONE!");
 
 
 
