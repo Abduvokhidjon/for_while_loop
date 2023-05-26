5e9f4a94bdce4358265004ba26ee19db146183e6
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-25 20:11:12 -0500

    first commit

---
+++ for_while_loop.js
@@ -0,0 +1,25 @@
+for (var i = 100; i > 0; i -= 2) {
+    console.log(i)
+}
+
+var i = 100
+
+while (i >= 1) {
+    console.log(i)
+    i--
+}
+
+while (i >= 1) {
+    console.log(i)
+    --i
+}
+
+while (i >= 1) {
+    console.log(i)
+    i = i - 1
+} 
+
+while (i >= 1) {
+    console.log(i)
+    i -= 1
+}
