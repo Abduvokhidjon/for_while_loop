92b0821957e10ef646f4db4ac6a803ec077dc3a3
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-31 15:33:41 -0500

    Added

--- nested_loop.js
+++ nested_loop.js
@@ -1,5 +1,24 @@
-for (x = 2; x <= 5; x++ ) {
-    for (y = 2; y <= 5; y++) {
-      console.log(x + " to the power of " + y + " is eaual to " + x**y)
-    }
+// for (x = 2; x <= 5; x++ ) {
+//     for (y = 2; y <= 5; y++) {
+//       console.log(x + " to the power of " + y + " is eaual to " + x**y)
+//     }
+// }
+
+
+// Homework for nested loop:
+
+// Print this as string or num. your choice:
+// 1
+// 22
+// 333
+// 4444
+// 55555
+var pattern = "";
+for (i = 1; i <= 5; i++) {
+    for (x = 1; x <= i; x++) {
+        pattern += i
+    } 
+    pattern += 
 }
+
+console.log(pattern)
